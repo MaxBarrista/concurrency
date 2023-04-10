9195f36961a84afeb1d1bb20efc8c304fbc332de
MaxBarrista <79321066+MaxBarrista@users.noreply.github.com>
2023-04-10 17:55:36 +0300

    feat: added test with a runnable object using lambda expression

--- src/main/java/com/barrista/concurrency/Main.java
+++ src/main/java/com/barrista/concurrency/Main.java
@@ -3,8 +3,19 @@ package com.barrista.concurrency;
 public class Main {
 
   public static void main(String[] args) {
-    // test1();
-    test2();
+//    test1();
+//    test2();
+    test3();
+  }
+
+  private static void test3() {
+    Runnable runnable = () -> {
+      System.out.println("This is runnable object instantiated using a lambda expression");
+      sleep(1500);
+      System.out.println("Test 3 finished.");
+    };
+    Thread thread = new Thread(runnable);
+    thread.start();
   }
 
   private static void test2() {
@@ -18,4 +29,11 @@ public class Main {
     myThread.start();
   }
 
+  private static void sleep(long milliseconds) {
+    try {
+      Thread.sleep(milliseconds);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+  }
 }
