24cbaf331505094fe27112a0f2d0d1c9dd47cb7f
MaxBarrista <79321066+MaxBarrista@users.noreply.github.com>
2023-04-13 17:55:56 +0300

    feat: added a test with a named thread with a runnable object using an anonymous class.

--- src/main/java/com/barrista/concurrency/Main.java
+++ src/main/java/com/barrista/concurrency/Main.java
@@ -5,7 +5,21 @@ public class Main {
   public static void main(String[] args) {
 //    test1();
 //    test2();
-    test3();
+//    test3();
+    test4();
+  }
+
+  private static void test4() {
+    Runnable runnable = new Runnable() {
+      @Override
+      public void run() {
+        System.out.println("A thread '"
+            + Thread.currentThread().getName()
+            + "' has started a runnable instantiated with an anonymous class");
+      }
+    };
+    Thread thread = new Thread(runnable, "123");
+    thread.start();
   }
 
   private static void test3() {
